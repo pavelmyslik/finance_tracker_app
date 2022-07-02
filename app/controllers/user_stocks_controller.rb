9f0de09a6fcbefc0d905150a3106b4a9bb8d57c2
pavelmyslik <pavel.myslik@email.cz>
2022-07-02 14:37:03 +0200

    Remove button for trakced stocks added

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,11 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.where(user_id: current_user.id, stock_id: stock.id).first.destroy
+    flash[:notice] = "Stock #{stock.name} was successfully removed from your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
